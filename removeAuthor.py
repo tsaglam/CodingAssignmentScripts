c7822f5839b7d58d4bcb5f39f08edd3da1d7b081
Timur Sağlam <timur.saglam@kit.edu>
2024-04-18 16:05:10 +0100

    Resolve pylint issues.

--- removeAuthor.py
+++ removeAuthor.py
@@ -1,21 +1,28 @@
 import os
 
 def remove_author_lines(file_path):
+    """
+    Remove lines containing '@author' from the specified file.
+    """
     print(file_path)
     lines_to_keep = []
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             if "@author" not in line:
                 lines_to_keep.append(line)
             else:
-                print(" - remove "+line)
+                print(" - remove " + line)
     
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         for line in lines_to_keep:
             file.write(line)
 
 def process_directory(directory):
-    for root, dirs, files in os.walk(directory):
+    """
+    Recursively process all files in the specified directory and its subdirectories,
+    removing lines containing '@author' from Java files.
+    """
+    for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             if os.path.isfile(file_path):
@@ -23,6 +30,6 @@ def process_directory(directory):
                     remove_author_lines(file_path)
 
 if __name__ == "__main__":
-    target_directory = "./submissions"  # Replace this with the path to your target directory
-    process_directory(target_directory)
+    TARGET_DIRECTORY = "./submissions"  # Replace this with the path to your target directory
+    process_directory(TARGET_DIRECTORY)
     print("Author lines have been removed from the files in the directory.")
